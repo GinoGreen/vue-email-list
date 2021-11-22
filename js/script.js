43587b4b99ea6ca8e1c96d068e0ed72883cf65f8
GinoGreen <luigiverde97@gmail.com>
2021-11-22 16:06:58 +0100

    soluzione stop if error

--- js/script.js
+++ js/script.js
@@ -20,19 +20,7 @@ const app = new Vue({
          
          this.isLoading = true;
 
-         for (let i = 0; i < this.MAIL_QUANTITY; i++) {
-         
-            // stampo mail solo se ho raccolto 'MAIL_QUANTITY' MAIL
-            if (!this.httpError) {
-               this.getFromApi();
-            } 
-         }
-         
-      },
-      
-      getFromApi() {
-         
-         axios.get('https://flynn.boolean.careers/exercises/api/random/mal')
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
          .then((response) => {
             
             const data = response.data;
@@ -42,7 +30,37 @@ const app = new Vue({
                this.isLoading = false;
                console.log('loading', this.isLoading);
             }
+            if (!this.httpError && this.isLoading) {
+               this.getFromApi();
+            }
+            
+         })
+         .catch((error) => {
+            console.log('errore http', error);
+            this.httpError = true;
+         });
    
+         // axios
+         // -> se vuoi continuare richami axios
+         //    -> se vuo continarue richiami axios
+         
+      },
+      
+      getFromApi() {
+         
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+         .then((response) => {
+            
+            const data = response.data;
+            
+            this.mails.push(data.response);
+            if (this.mails.length === this.MAIL_QUANTITY) {
+               this.isLoading = false;
+               console.log('loading', this.isLoading);
+            }
+            if (!this.httpError && this.isLoading) {
+               this.getMails();
+            } 
          })
          .catch((error) => {
             console.log('errore http', error);
